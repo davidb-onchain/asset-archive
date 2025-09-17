1771595f730c5687d386854b8251b15336a93d7a
david <davidjbailey.public@gmail.com>
2025-09-16 20:36:30 -0400

    fix: Correct Terraform S3 backend configuration

--- deploy/public-dev/infra/backend.tf
+++ deploy/public-dev/infra/backend.tf
@@ -5,13 +5,16 @@ terraform {
     #    It will look like: https://<bucket-name>.<region>.digitaloceanspaces.com
     # 2. Fill in the placeholders below using the values from the URL.
 
-    endpoint = "nyc3.digitaloceanspaces.com" # e.g., "nyc3.digitaloceanspaces.com"
-    region   = "nyc3"                       # e.g., "nyc3"
-    bucket   = "state-space"                  # e.g., "your-tf-state-bucket"
+    endpoints = {
+      s3 = "https://nyc3.digitaloceanspaces.com"
+    }
+    region   = "us-east-1"                      # Must be a valid AWS region, but is ignored by DO.
+    bucket   = "state-space"                    # e.g., "your-tf-state-bucket"
     key      = "dev/terraform.tfstate"
 
     # These settings are required for DigitalOcean Spaces.
     skip_credentials_validation = true
     skip_metadata_api_check     = true
+    skip_region_validation      = true
   }
 }
